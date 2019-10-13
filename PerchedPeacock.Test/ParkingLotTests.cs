0e2961eb9b758b3a4ef6d41e5b836f11fc34baf5
harishsubash <harishbabu_s@live.com>
2019-10-13 18:26:08 +0530

    Framework update to v2.1 for Test project.

--- PerchedPeacock.Test/ParkingLotTests.cs
+++ PerchedPeacock.Test/ParkingLotTests.cs
@@ -9,7 +9,7 @@ namespace PerchedPeacock.Test
         private ParkingLot _parkingLot;
 
         [Test]
-        public void Contains_ParkingLots()
+        public void create_parkinglot_contains_allocated_lots()
         {
             _parkingLot = new ParkingLot(Guid.NewGuid(), "ParkingLot1", "ITPL, Bangalore", 5);
             Assert.AreEqual(_parkingLot.NumberOfSlots, _parkingLot.ParkingSlots.Count);
